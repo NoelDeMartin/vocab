3a6fd0540db470ad7a5c83dd24c386af40f5ca02
Noel De Martin <noel@noeldemartin.com>
2022-12-06 17:27:32 +0100

    Configure static analysis with Larastan

--- config/ontologies.php
+++ config/ontologies.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'cache_ttl' => env('ONTOLOGIES_CACHE_TTL', 3600),
+    'cache_ttl' => intval(env('ONTOLOGIES_CACHE_TTL', 3600)),
     'base_uri' => env('ONTOLOGIES_BASE_URI', 'https://vocab.noeldemartin.com/'),
 ];
