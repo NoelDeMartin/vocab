bd567c6aadba1021d06bc1db3ea324f8012779dc
Noel De Martin <noel@noeldemartin.com>
2026-09-03 17:21:48 +0200

    Fix CI

--- config/cache.php
+++ config/cache.php
@@ -17,7 +17,7 @@ return [
 
     'default' => env('CACHE_STORE', env('CACHE_DRIVER', 'file')),
 
-    'serializable_classes' => false,
+    'serializable_classes' => true,
 
     /*
     |--------------------------------------------------------------------------
